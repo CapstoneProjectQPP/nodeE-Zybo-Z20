1503b20e143698d4518802b18505287966a9edcc
Iyury1 <iyurychu@ualberta.ca>
2023-04-14 11:28:49 -0600

    Completed QPP for HLS and CPP

---
+++ Linux/NodeE-CPP/include/Constants.h
@@ -0,0 +1,12 @@
+#pragma once
+
+namespace Xceed {
+    namespace Constants {
+        const int n = 2;                                    // Must be a factor of 8
+        const int M = 5;
+        const int mat_size = 1 << n;
+
+        const uint8_t mask = ((uint8_t)(-1)) << (8 - n);
+        const uint16_t block_size = 1024;                    // In byte
+    }
+}
